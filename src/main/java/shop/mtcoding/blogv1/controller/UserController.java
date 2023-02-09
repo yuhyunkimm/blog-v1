e2b43fc0931302895d4ae90bd8aabce47eb423a7
YouHyunKim <yuhyunk7@gmail.com>
2023-02-09 16:28:21 +0900

    회원가입 test 완료

--- src/main/java/shop/mtcoding/blogv1/controller/UserController.java
+++ src/main/java/shop/mtcoding/blogv1/controller/UserController.java
@@ -1,29 +1,33 @@
 package shop.mtcoding.blogv1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import shop.mtcoding.blogv1.dto.user.userReq.JoinReqDto;
+import shop.mtcoding.blogv1.handler.ex.CustomException;
+import shop.mtcoding.blogv1.service.UserService;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/join")
     public String join(JoinReqDto joinReqDto) {
         if (joinReqDto.getUsername() == null || joinReqDto.getUsername().isEmpty()) {
-            System.out.println(joinReqDto.getUsername());
-
+            throw new CustomException("username을 작성해주세요");
         }
         if (joinReqDto.getPassword() == null || joinReqDto.getPassword().isEmpty()) {
-            System.out.println(joinReqDto.getPassword());
-
+            throw new CustomException("password을 작성해주세요");
         }
         if (joinReqDto.getEmail() == null || joinReqDto.getEmail().isEmpty()) {
-            System.out.println(joinReqDto.getEmail());
-
+            throw new CustomException("email을 작성해주세요");
         }
-        userService.회원가입(joinReqDto);
-
+        int result = userService.회원가입(joinReqDto);
+        if (result != 1) {
+            throw new CustomException("회원가입실패");
+        }
         return "redirect:/loginForm";
     }
 
---
+++ src/main/java/shop/mtcoding/blogv1/service/UserService.java
@@ -0,0 +1,9 @@
+package shop.mtcoding.blogv1.service;
+
+import shop.mtcoding.blogv1.dto.user.userReq.JoinReqDto;
+
+public class UserService {
+    public int 회원가입(JoinReqDto joinReqDto) {
+        return -1;
+    }
+}
