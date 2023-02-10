c5478171bd0cf5f9de065081253dbd5a3464632a
unknown <yuhyunk7@gmail.com>
2023-02-11 01:20:57 +0900

    글 수정 기능 완료

--- src/main/java/shop/mtcoding/blogv1/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blogv1/controller/BoardController.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blogv1.controller;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +12,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import shop.mtcoding.blogv1.dto.ResponsDto;
 import shop.mtcoding.blogv1.dto.board.BoardReq.BoardSaveReqDto;
+import shop.mtcoding.blogv1.dto.board.BoardReq.BoardUpdateReqDto;
 import shop.mtcoding.blogv1.handler.ex.CustomApiException;
 import shop.mtcoding.blogv1.handler.ex.CustomException;
+import shop.mtcoding.blogv1.model.Board;
 import shop.mtcoding.blogv1.model.BoardRepository;
 import shop.mtcoding.blogv1.model.User;
 import shop.mtcoding.blogv1.service.BoardService;
@@ -32,6 +37,24 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @PutMapping("/board/{id}")
+    public @ResponseBody ResponseEntity<?> update(@PathVariable int id,
+            @RequestBody BoardUpdateReqDto boardUpdateReqDto, HttpServletResponse response) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomApiException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+        }
+        if (boardUpdateReqDto.getTitle() == null || boardUpdateReqDto.getTitle().isEmpty()) {
+            throw new CustomApiException("title을 작성해주세요");
+        }
+        if (boardUpdateReqDto.getContent() == null || boardUpdateReqDto.getContent().isEmpty()) {
+            throw new CustomApiException("content를 작성해주세요");
+        }
+        boardService.게시글수정(id, boardUpdateReqDto, principal.getId());
+
+        return new ResponseEntity<>(new ResponsDto<>(1, "게시글수정성공", null), HttpStatus.OK);
+    }
 
     @DeleteMapping("/board/{id}")
     public @ResponseBody ResponseEntity<?> delete(@PathVariable int id) {
@@ -86,7 +109,20 @@ public class BoardController {
     }
 
     @GetMapping("/board/{id}/UpdateForm")
-    public String boardUpdateForm(@PathVariable int id) {
+    public String boardUpdateForm(@PathVariable int id, Model model) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomApiException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+        }
+        Board boardPS = boardRepository.findById(id);
+        if (boardPS == null) {
+            throw new CustomException("없는 게시글을 수정할 수 없습니다.");
+        }
+        if (boardPS.getUserId() != principal.getId()) {
+            throw new CustomException("게시글을 수정할 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+        model.addAttribute("board", boardPS);
         return "board/boardUpdateForm";
+
     }
 }
--- src/main/java/shop/mtcoding/blogv1/service/BoardService.java
+++ src/main/java/shop/mtcoding/blogv1/service/BoardService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.blogv1.dto.board.BoardReq.BoardSaveReqDto;
+import shop.mtcoding.blogv1.dto.board.BoardReq.BoardUpdateReqDto;
 import shop.mtcoding.blogv1.handler.ex.CustomApiException;
 import shop.mtcoding.blogv1.handler.ex.CustomException;
 import shop.mtcoding.blogv1.model.Board;
@@ -45,6 +46,22 @@ public class BoardService {
         } catch (Exception e) {
             throw new CustomApiException("서버에 일시적인 문제가 생겼습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
+        }
+    }
+
+    @Transactional
+    public void 게시글수정(int id, BoardUpdateReqDto boardUpdateReqDto, int principalId) {
+        Board boardPS = boardRepository.findById(id);
+        if (boardPS == null) {
+            throw new CustomApiException("해당 게시글을 찾을 수 없습니다");
+        }
+        if (boardPS.getUserId() != principalId) {
+            throw new CustomApiException("게시글을 수정할 권한이 없습니다", HttpStatus.FORBIDDEN);
+        }
+
+        int result = boardRepository.updateById(id, boardUpdateReqDto.getTitle(), boardUpdateReqDto.getContent());
+        if (result != 1) {
+            throw new CustomApiException("게시글을 수정에 실패하였습니다", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
